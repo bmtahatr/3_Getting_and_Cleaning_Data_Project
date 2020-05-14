1828d404d4d9044de604d4b27c6bf6902b482d55
Mohame T <mohamed.t.bennai@gmail.com>
2020-05-14 21:50:56 +0200

    Adding the files

---
+++ Readme.md
@@ -0,0 +1,18 @@
+---
+title: "Readme"
+author: "Mohamed Tahar Bennai"
+date: "14/05/2020"
+output: html_document
+---
+
+## Summary
+This project was made to process a messy dataset and transform it into a tidy tadaset. This trasformation was done using an R script (run_analysis.R), and the final result can be found in "tidyData.txt". The "Cookbook.md" document describes the information presented in "tidyData.txt".
+
+## Script description
+
+1. The first section is dedicated to the download and unzip the of dataset:
+2. The second section is dedicated to the load of the anted features
+3. The third section is dedicated to the load of the train datasets
+4. The forth is dedicated to the load of the test datasets
+5. The fifth is dedicated to the merge of the train and test datasets
+6. Lastly, an instruction is dedecated to the generation of the new tidy formated dataset
---
+++ run_analysis.R
@@ -0,0 +1,35 @@
+# This section is dedicated to the download and unzip the of dataset:
+if (!file.exists(filename)){
+    fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+    download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+    unzip(filename) 
+}
+# This section is dedicated to the load of the anted features
+packages <- c("data.table", "reshape2")
+activityLabels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt"), col.names = c("classLabels", "activityName"))
+features <- fread(file.path(path, "features.txt"), col.names = c("index", "featureNames"))
+featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
+measurements <- features[featuresWanted, featureNames]
+measurements <- gsub('[()]', '', measurements)
+# This section is dedicated to the load of the train datasets
+train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE] 
+data.table::setnames(train, colnames(train), measurements)
+trainActivities <- fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt"), col.names = c("Activity"))
+trainSubjects <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt"), col.names = c("SubjectNum"))
+# This section is dedicated to the load of the test datasets
+test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
+data.table::setnames(test, colnames(test), measurements)
+testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt"), col.names = c("Activity"))
+testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt"), col.names = c("SubjectNum"))
+test <- cbind(testSubjects, testActivities, test)
+# This section is dedicated to the merge of the train and test datasets
+combined <- rbind(train, test)
+combined[["Activity"]] <- factor(combined[, Activity], levels = activityLabels[["classLabels"]]
+                                  , labels = activityLabels[["activityName"]])
+combined[["SubjectNum"]] <- as.factor(combined[, SubjectNum])
+combined <- reshape2::melt(data = combined, id = c("SubjectNum", "Activity"))
+combined <- reshape2::dcast(data = combined, SubjectNum + Activity ~ variable, fun.aggregate = mean)
+# This instruction is dedecated to the generation of the new tidy formated dataset
+data.table::fwrite(x = combined, file = "tidyData.txt", quote = FALSE)
